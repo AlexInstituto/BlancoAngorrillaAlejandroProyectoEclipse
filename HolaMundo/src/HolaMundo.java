7b050b59c964d633f4adc905b6f69305bc298938
Alejandro Angorrilla Blanco <aangbla317@g.educaand.es>
2025-04-01 20:06:13 +0200

    Update HolaMundo.java

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	// Tu programa comienza con una llamada a main().
 	 // Imprime "Hola Mundo" a la ventana de la	terminal.
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo desde GitHub");
 	}
 
 }
